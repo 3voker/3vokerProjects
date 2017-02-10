bbd64529e2c7cf04b19d00220702bed2cf5d5946
3voker <DeAndre.Owens@iam.local>
2017-02-09 21:35:08 -0600

    Tried adding the randomized level.

--- DoYouDeliver/Assets/Scripts/QuestTriggers.cs
+++ DoYouDeliver/Assets/Scripts/QuestTriggers.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class QuestTriggers : MonoBehaviour {
 
@@ -12,73 +13,125 @@ public class QuestTriggers : MonoBehaviour {
     [SerializeField]
     GameObject dropOffPoint;
 
-    [SerializeField]
-    GameObject key;
+    public int newLocation;
+    //[SerializeField]
+    //GameObject key;
 
     [SerializeField]
     Camera mainCamera;
 
+    //[SerializeField]
+    //GameObject[] locations;
     [SerializeField]
-    Camera cutSceneCamera;
+    Vector3[] locations;
 
-    [SerializeField]
-    private int numOfEnemies;
-    [SerializeField]
-    Animator anim;
+    //[SerializeField]
+    //Camera cutSceneCamera;
 
-    AudioSource audioSource;
+    //[SerializeField]
+    //private int numOfEnemies;
+    //[SerializeField]
+    //Animator anim;
 
-  
+    //AudioSource audioSource;
+
+
 
     void Start()
     {
         //anim = GetComponent<Animation>();
         //key.SetActive(true);
-        mainCamera.enabled = true;
-        cutSceneCamera.enabled = false;
-        
-        
+        //mainCamera.enabled = true;
+        //cutSceneCamera.enabled = false;
+
+        NextMission();
     }
     void Update()
     {
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Done playing"))
-        {
-            key.SetActive(false);
-            mainCamera.enabled = true;
-            Destroy(cutSceneCamera);
-            Destroy(gameObject);
+
+        CheckMission();
+        //if (anim.GetCurrentAnimatorStateInfo(0).IsName("Done playing"))
+        //{
+        //    key.SetActive(false);
+        //    mainCamera.enabled = true;
+        //    Destroy(cutSceneCamera);
+        //    Destroy(gameObject);
            
-           // key.SetActive(false);
-        }
+        //   // key.SetActive(false);
+        //}
     }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            mainCamera.enabled = false;
-            cutSceneCamera.enabled = true;
-            anim.SetBool("isTriggered", true);
-           
+            if(this.gameObject == homePoint)
+            {
+                BossFeedBack();
+                NextMission();
+            }
+            else if (this.gameObject == dropOffPoint)
+            {
+                CustomerFeedBack();
+                ReturnToBase();
+            }
 
+            //mainCamera.enabled = false;
+            //cutSceneCamera.enabled = true;
+            //anim.SetBool("isTriggered", true);
             //anim["cutsceneAnimation"].wrapMode = WrapMode.Once;
-          // anim.Play("cutsceneAnimation");
+            // anim.Play("cutsceneAnimation");
             //if (anim.clip("cutsceneAnimation"))
             //transform(0, 0, 0);
         }
         //Destroy(other.gameObject);
     }
-  /*  EnemySpawn()
+
+    private void BossFeedBack()
     {
-        for (int i = 0; i< 3; i++)
-                { 		
-              Instantiate(enemyPrefab, new Vector3(i* 2.0f, 0, player + 10), Quaternion.identity);
-               // Instantiate(EnemyPrefab)
-               }
+        throw new NotImplementedException();
+    }
 
-                if (numOfEnemies == 0)
-            {
-                
-            } 
+    private void ReturnToBase()
+    {
+        throw new NotImplementedException();
+    }
 
-			} */
+    private void CustomerFeedBack()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void NextMission()
+    {
+        //Randomizes new location
+        newLocation = UnityEngine.Random.Range(0, locations.Length); 
+        //Location selected   
+        locations[newLocation] = new Vector3 current;
+        
+        Debug.Log("New Location: " + newLocation);   
+    }
+    private void CheckMission(Vector3 current)
+    {
+        if(newLocation == current)
+        {
+            NextMission();
+            Instantiate(dropOffPoint, current, Quaternion.identity);
+            
+        }
+     
+    }
+    /*  EnemySpawn()
+ {
+     for (int i = 0; i< 3; i++)
+             { 		
+           Instantiate(enemyPrefab, new Vector3(i* 2.0f, 0, player + 10), Quaternion.identity);
+            // Instantiate(EnemyPrefab)
+            }
+
+             if (numOfEnemies == 0)
+         {
+
+         } 
+
+         } */
 }
