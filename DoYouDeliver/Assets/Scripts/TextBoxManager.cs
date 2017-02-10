51e9b0367d508e5ba454aa5f3602708d7007464d
3voker <Drechristophe@gmail.com>
2017-02-10 16:23:31 -0600

    Text Importer and Text Box Manager
    Added a intro scene with text. Added text box, along with scripts for
    it. Created Customer Call texts.

---
+++ DoYouDeliver/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+    GameObject textBox;
+
+    [SerializeField]
+    Text theText;
+    [SerializeField]
+    TextAsset textFile;
+
+    [SerializeField]
+    string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+   // public PlayerController player;
+
+    void Start()
+    {
+        //player = FindObjectOfType<PlayerController>();  
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+        if(endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theText.text = textLines[currentLine];
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            currentLine++;
+        }
+        if(currentLine > endAtLine)
+        {
+            textBox.SetActive(false);
+        }
+    }
+}
