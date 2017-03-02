7af920df3f67a74294f18c67e31cb1ddd87fadfc
3voker <DeAndre.Owens@iam.local>
2017-03-01 21:05:39 -0600

    Party Formation tweaks
    Added four useless Party Formation scripts. Try to work on the collision
    for player placements. Also make sure that current canvas's are workable
    at their current position.

---
+++ Might of Akelia/Assets/Scripts/Interface/PartyFormationManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartyFormationManager : MonoBehaviour {
+
+	// Use this for initialization
+	[SerializeField]
+	GameObject[,] partyFormationSpots; 
+
+	[SerializeField]
+	int seeMe;
+
+
+	void Start(){
+
+
+
+	}
+
+}
---
+++ Might of Akelia/Assets/Scripts/Interface/PartyFormationTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartyFormationTrigger : MonoBehaviour {
+
+	// Use this for initialization
+
+	bool isOnTrigger = false; 
+	void OnTriggerStay(GameObject other){
+		yield return WaitForSeconds (WaitForSeconds: 1);
+		if (other.gameObject.tag == "Player") {
+			isOnTrigger = true;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		triggerCheck();
+	}
+	void triggerCheck(){
+
+
+			}
+}
